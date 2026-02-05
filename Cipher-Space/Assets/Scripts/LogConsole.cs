0b01baa7ff1b6fa8a8903e3fc1e79e34d086301d
zndayeh1229 <zndayeh1229@gmail.com>
2026-02-05 16:10:05 -0500

    Player movement and button events script, working player prefab

---
+++ Cipher-Space/Assets/Scripts/LogConsole.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LogConsole : MonoBehaviour
+{
+    public void LogMessage()
+    {
+        Debug.Log("Successfully logged!"); // Logs the provided message to the Unity console
+    }
+}
