7d63809f8004df5cb3e909e9e055cf555f18b811
zndayeh1229 <zndayeh1229@gmail.com>
2026-02-17 12:43:57 -0500

    added trigger events

--- Cipher-Space/Assets/Scripts/TriggerEvent.cs
+++ Cipher-Space/Assets/Scripts/TriggerEvent.cs
@@ -1,37 +1,22 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider2D))] // Ensures that a Collider2D component is attached to the GameObject
-public class ButtonEvent : MonoBehaviour
+public class TriggerEvent : MonoBehaviour
 {
-
     [Header("Tag Settings")]
     public string tagName = "Player"; // Tag name to identify the player
 
-    [Header("Button Event Settings")]
-    public UnityEvent onButtonPressed; // Event to invoke when button is pressed
+    [Header("Trigger Event Settings")]
+    public UnityEvent onTriggerEnter; // Event to invoke when player enters the trigger
+    public UnityEvent onTriggerExit; // Event to invoke when player exits the trigger
     private bool isInTrigger = false; // Tracks if the player is in the trigger area
-
-    void Update()
-    {
-        if (Keyboard.current.eKey.wasPressedThisFrame && isInTrigger) // Check if 'E' key was pressed this frame and player is in trigger
-        {
-            onButtonPressed.Invoke(); // Invoke the button pressed event
-        }
-        else
-        {
-            return;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag(tagName)) // Check if the colliding object has the specified tag
         {
             isInTrigger = true;
         }
-
     }
 
     private void OnTriggerExit2D(Collider2D collider)
@@ -39,6 +24,18 @@ public class ButtonEvent : MonoBehaviour
         if (collider.CompareTag(tagName)) // Check if the exiting object has the specified tag
         {
             isInTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isInTrigger)
+        {
+            onTriggerEnter.Invoke(); // Invoke the button pressed event
+        }
+        else
+        {
+            onTriggerExit.Invoke(); // Invoke the button pressed event
         }
     }
 }
