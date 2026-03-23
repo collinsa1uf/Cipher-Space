889f65a26d1d13c0167f89d6be424a16930eb8f4
GrDSM <dscott.michaud@ufl.edu>
2026-03-23 19:36:37 -0400

    Levers and Buttons

--- Backend/AI/test.py
+++ Backend/AI/test.py
@@ -60,28 +60,6 @@ def object_call(word, prompt, incorrect_items):
 
         content = AI_implementation(prompt)
 
-        messages = [{"role": "user", "content": prompt}]
-        text = tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True,
-            enable_thinking=False
-        )
-        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=32768
-        )
-        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
-
-        try:
-            index = len(output_ids) - output_ids[::-1].index(151668)
-        except ValueError:
-            index = 0
-
-        content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
-
         pattern = r'\*\*(.*?)\*\*'
         list_items = re.findall(pattern, content)
         if len(list_items) == 0:
